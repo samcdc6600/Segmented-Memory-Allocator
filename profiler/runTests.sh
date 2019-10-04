cd682d46d9c53a39cea1555451e7c5af41c575ed
SB <s3545852@student.rmit.edu.au>
2019-10-05 09:55:01 +1000

    Renamed new improved script from "test.sh" to "runTests.sh" and removed "runTestsPrintStats.sh".

--- profiler/runTests.sh
+++ profiler/runTests.sh
@@ -1,208 +1,24 @@
 #!/bin/sh
-# Test 0
-#	Policy 0
-./profiler 0 0 0 0 > testresults/0/noStats/0000 
-./profiler 0 0 1 0 > testresults/0/noStats/0010 
-./profiler 0 0 2 0 > testresults/0/noStats/0020 
-./profiler 0 0 3 0 > testresults/0/noStats/0030 
-./profiler 0 0 4 0 > testresults/0/noStats/0040 
-#./profiler 0 0 5 0 > testresults/0/noStats/0050 
-#./profiler 0 0 6 0 > testresults/0/noStats/0060 
-#	Policy 1
-./profiler 1 0 0 0 > testresults/0/noStats/1000 
-./profiler 1 0 1 0 > testresults/0/noStats/1010 
-./profiler 1 0 2 0 > testresults/0/noStats/1020 
-./profiler 1 0 3 0 > testresults/0/noStats/1030 
-./profiler 1 0 4 0 > testresults/0/noStats/1040 
-#./profiler 1 0 5 0 > testresults/0/noStats/1050 
-#./profiler 1 0 6 0 > testresults/0/noStats/1060 
-#	Policy 2
-./profiler 2 0 0 0 > testresults/0/noStats/2000 
-./profiler 2 0 1 0 > testresults/0/noStats/2010 
-./profiler 2 0 2 0 > testresults/0/noStats/2020 
-./profiler 2 0 3 0 > testresults/0/noStats/2030 
-./profiler 2 0 4 0 > testresults/0/noStats/2040 
-#./profiler 2 0 5 0 > testresults/0/noStats/2050 
-#./profiler 2 0 6 0 > testresults/0/noStats/2060 
 
-# Test 1
-#	Policy 0
-./profiler 0 1 0 0 > testresults/1/noStats/0100 
-./profiler 0 1 1 0 > testresults/1/noStats/0110 
-./profiler 0 1 2 0 > testresults/1/noStats/0120 
-./profiler 0 1 3 0 > testresults/1/noStats/0130 
-./profiler 0 1 4 0 > testresults/1/noStats/0140 
-#./profiler 0 1 5 0 > testresults/1/noStats/0150 
-#./profiler 0 1 6 0 > testresults/1/noStats/0160 
-#	Policy 1
-./profiler 1 1 0 0 > testresults/1/noStats/1100 
-./profiler 1 1 1 0 > testresults/1/noStats/1110 
-./profiler 1 1 2 0 > testresults/1/noStats/1120 
-./profiler 1 1 3 0 > testresults/1/noStats/1130 
-./profiler 1 1 4 0 > testresults/1/noStats/1140 
-#./profiler 1 1 5 0 > testresults/1/noStats/1150 
-#./profiler 1 1 6 0 > testresults/1/noStats/1160 
-#	Policy 2
-./profiler 2 1 0 0 > testresults/1/noStats/2100 
-./profiler 2 1 1 0 > testresults/1/noStats/2110 
-./profiler 2 1 2 0 > testresults/1/noStats/2120 
-./profiler 2 1 3 0 > testresults/1/noStats/2130 
-./profiler 2 1 4 0 > testresults/1/noStats/2140 
-#./profiler 2 1 5 0 > testresults/1/noStats/2150 
-#./profiler 2 1 6 0 > testresults/1/noStats/2160 
-
-# Test 2
-#	Policy 0
-./profiler 0 2 0 0 > testresults/2/noStats/0200 
-./profiler 0 2 1 0 > testresults/2/noStats/0210 
-./profiler 0 2 2 0 > testresults/2/noStats/0220 
-./profiler 0 2 3 0 > testresults/2/noStats/0230 
-./profiler 0 2 4 0 > testresults/2/noStats/0240 
-#./profiler 0 2 5 0 > testresults/2/noStats/0250 
-#./profiler 0 2 6 0 > testresults/2/noStats/0260 
-#	Policy 1
-./profiler 1 2 0 0 > testresults/2/noStats/1200 
-./profiler 1 2 1 0 > testresults/2/noStats/1210 
-./profiler 1 2 2 0 > testresults/2/noStats/1220 
-./profiler 1 2 3 0 > testresults/2/noStats/1230 
-./profiler 1 2 4 0 > testresults/2/noStats/1240 
-#./profiler 1 2 5 0 > testresults/2/noStats/1250 
-#./profiler 1 2 6 0 > testresults/2/noStats/1260 
-#	Policy 2
-./profiler 2 2 0 0 > testresults/2/noStats/2200 
-./profiler 2 2 1 0 > testresults/2/noStats/2210 
-./profiler 2 2 2 0 > testresults/2/noStats/2220 
-./profiler 2 2 3 0 > testresults/2/noStats/2230 
-./profiler 2 2 4 0 > testresults/2/noStats/2240 
-#./profiler 2 2 5 0 > testresults/2/noStats/2250 
-#./profiler 2 2 6 0 > testresults/2/noStats/2260 
-
-# Test 3
-#	Policy 0
-./profiler 0 3 0 0 > testresults/3/noStats/0300 
-./profiler 0 3 1 0 > testresults/3/noStats/0310 
-./profiler 0 3 2 0 > testresults/3/noStats/0320 
-./profiler 0 3 3 0 > testresults/3/noStats/0330 
-./profiler 0 3 4 0 > testresults/3/noStats/0340 
-#./profiler 0 3 5 0 > testresults/3/noStats/0350 
-#./profiler 0 3 6 0 > testresults/3/noStats/0360 
-#	Policy 1
-./profiler 1 3 0 0 > testresults/3/noStats/1300 
-./profiler 1 3 1 0 > testresults/3/noStats/1310 
-./profiler 1 3 2 0 > testresults/3/noStats/1320 
-./profiler 1 3 3 0 > testresults/3/noStats/1330 
-./profiler 1 3 4 0 > testresults/3/noStats/1340 
-#./profiler 1 3 5 0 > testresults/3/noStats/1350 
-#./profiler 1 3 6 0 > testresults/3/noStats/1360 
-#	Policy 2
-./profiler 2 3 0 0 > testresults/3/noStats/2300 
-./profiler 2 3 1 0 > testresults/3/noStats/2310 
-./profiler 2 3 2 0 > testresults/3/noStats/2320 
-./profiler 2 3 3 0 > testresults/3/noStats/2330 
-./profiler 2 3 4 0 > testresults/3/noStats/2340 
-#./profiler 2 3 5 0 > testresults/3/noStats/2350 
-#./profiler 2 3 6 0 > testresults/3/noStats/2360 
-
-# Test 4
-#	Policy 0
-./profiler 0 4 0 0 > testresults/4/noStats/0400 
-./profiler 0 4 1 0 > testresults/4/noStats/0410 
-./profiler 0 4 2 0 > testresults/4/noStats/0420 
-./profiler 0 4 3 0 > testresults/4/noStats/0430 
-./profiler 0 4 4 0 > testresults/4/noStats/0440 
-#./profiler 0 4 5 0 > testresults/4/noStats/0450 
-#./profiler 0 4 6 0 > testresults/4/noStats/0460 
-#	Policy 1
-./profiler 1 4 0 0 > testresults/4/noStats/1400 
-./profiler 1 4 1 0 > testresults/4/noStats/1410 
-./profiler 1 4 2 0 > testresults/4/noStats/1420 
-./profiler 1 4 3 0 > testresults/4/noStats/1430 
-./profiler 1 4 4 0 > testresults/4/noStats/1440 
-#./profiler 1 4 5 0 > testresults/4/noStats/1450 
-#./profiler 1 4 6 0 > testresults/4/noStats/1460 
-#	Policy 2
-./profiler 2 4 0 0 > testresults/4/noStats/2400 
-./profiler 2 4 1 0 > testresults/4/noStats/2410 
-./profiler 2 4 2 0 > testresults/4/noStats/2420 
-./profiler 2 4 3 0 > testresults/4/noStats/2430 
-./profiler 2 4 4 0 > testresults/4/noStats/2440 
-#./profiler 2 4 5 0 > testresults/4/noStats/2450 
-#./profiler 2 4 6 0 > testresults/4/noStats/2460 
-
-# Test 5
-#	Policy 0
-./profiler 0 5 0 0 > testresults/5/noStats/0500 
-./profiler 0 5 1 0 > testresults/5/noStats/0510 
-./profiler 0 5 2 0 > testresults/5/noStats/0520 
-./profiler 0 5 3 0 > testresults/5/noStats/0530 
-./profiler 0 5 4 0 > testresults/5/noStats/0540 
-#./profiler 0 5 5 0 > testresults/5/noStats/0550 
-#./profiler 0 5 6 0 > testresults/5/noStats/0560 
-#	Policy 1
-./profiler 1 5 0 0 > testresults/5/noStats/1500 
-./profiler 1 5 1 0 > testresults/5/noStats/1510 
-./profiler 1 5 2 0 > testresults/5/noStats/1520 
-./profiler 1 5 3 0 > testresults/5/noStats/1530 
-./profiler 1 5 4 0 > testresults/5/noStats/1540 
-#./profiler 1 5 5 0 > testresults/5/noStats/1550 
-#./profiler 1 5 6 0 > testresults/5/noStats/1560 
-#	Policy 2
-./profiler 2 5 0 0 > testresults/5/noStats/2500 
-./profiler 2 5 1 0 > testresults/5/noStats/2510 
-./profiler 2 5 2 0 > testresults/5/noStats/2520 
-./profiler 2 5 3 0 > testresults/5/noStats/2530 
-./profiler 2 5 4 0 > testresults/5/noStats/2540 
-#./profiler 2 5 5 0 > testresults/5/noStats/2550 
-#./profiler 2 5 6 0 > testresults/5/noStats/2560 
-
-# Test 6
-#	Policy 0
-./profiler 0 6 0 0 > testresults/6/noStats/0600 
-./profiler 0 6 1 0 > testresults/6/noStats/0610 
-./profiler 0 6 2 0 > testresults/6/noStats/0620 
-./profiler 0 6 3 0 > testresults/6/noStats/0630 
-./profiler 0 6 4 0 > testresults/6/noStats/0640 
-#./profiler 0 6 5 0 > testresults/6/noStats/0650 
-#./profiler 0 6 6 0 > testresults/6/noStats/0660 
-#	Policy 1
-./profiler 1 6 0 0 > testresults/6/noStats/1600 
-./profiler 1 6 1 0 > testresults/6/noStats/1610 
-./profiler 1 6 2 0 > testresults/6/noStats/1620 
-./profiler 1 6 3 0 > testresults/6/noStats/1630 
-./profiler 1 6 4 0 > testresults/6/noStats/1640 
-#./profiler 1 6 5 0 > testresults/6/noStats/1650 
-#./profiler 1 6 6 0 > testresults/6/noStats/1660 
-#	Policy 2 
-./profiler 2 6 0 0 > testresults/6/noStats/2600 
-./profiler 2 6 1 0 > testresults/6/noStats/2610 
-./profiler 2 6 2 0 > testresults/6/noStats/2620 
-./profiler 2 6 3 0 > testresults/6/noStats/2630 
-./profiler 2 6 4 0 > testresults/6/noStats/2640 
-#./profiler 2 6 5 0 > testresults/6/noStats/2650 
-#./profiler 2 6 6 0 > testresults/6/noStats/2660 
-
-# Test 7
-#	Policy 0
-./profiler 0 7 0 0 > testresults/7/noStats/0700 
-./profiler 0 7 1 0 > testresults/7/noStats/0710 
-./profiler 0 7 2 0 > testresults/7/noStats/0720 
-./profiler 0 7 3 0 > testresults/7/noStats/0730 
-./profiler 0 7 4 0 > testresults/7/noStats/0740 
-#./profiler 0 7 5 0 > testresults/7/noStats/0550 
-#./profiler 0 7 6 0 > testresults/7/noStats/0560 
-#	Policy 1 
-./profiler 1 7 0 0 > testresults/7/noStats/1700 
-./profiler 1 7 1 0 > testresults/7/noStats/1710 
-./profiler 1 7 2 0 > testresults/7/noStats/1720 
-./profiler 1 7 3 0 > testresults/7/noStats/1730 
-./profiler 1 7 4 0 > testresults/7/noStats/1740 
-#./profiler 1 7 5 0 > testresults/7/noStats/1750 
-#./profiler 1 7 6 0 > testresults/7/noStats/1760 
-#	Policy 2
-./profiler 2 7 0 0 > testresults/7/noStats/2700 
-./profiler 2 7 1 0 > testresults/7/noStats/2710 
-./profiler 2 7 2 0 > testresults/7/noStats/2720 
-./profiler 2 7 3 0 > testresults/7/noStats/2730 
-./profiler 2 7 4 0 > testresults/7/noStats/2740 
-#./profiler 2 7 5 0 > testresults/7/noStats/2750 
-#./profiler 2 7 6 0 > testresults/7/noStats/2760 
+for policy in 0 1 2			# Memory allocation policy, where the range is [0, 2]
+do
+    for testNum in 0 1 2 3 4 5 6 7 # Specific test, where the range is [0, 7]
+    do
+	for sizeNum in 0 1 2 3 4 #5 6 # Number of allocations to perform, where the range is [0, 6]
+	do
+	    for listStat in 0 1	# Output extra statistics about the inUse and holes lists when 1. There is a choice
+	    do			# here because there is a run time overhead associated with gathering the extra
+		# statistics that affects the other statistics.
+		if test $listStat -eq 0
+		then
+		    STATS="normalStats"
+		else
+		    STATS="extraStats"
+		fi
+		mkdir -p testresults/$testNum/$STATS # Make directory
+		# Run tests
+		./profiler $policy $testNum $sizeNum $listStat > testresults/$testNum/$STATS/$policy$testNum$sizeNum$listStat
+	    done
+	    done
+    done
+done
